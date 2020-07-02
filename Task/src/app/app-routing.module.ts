6841e8eb07796ca7cf485bf8087e8fd5607b9767
shivam <shivambansa909@gmail.com>
2020-07-02 21:17:37 +0530

    latest code

--- Task/src/app/app-routing.module.ts
+++ Task/src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
+import { AuthGuard } from './auth.guard';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:  '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path : 'register',
+    component : RegistrationComponent
+  },
+  {
+    path : 'login',
+    component : LoginComponent
+  },
+  {
+    path : 'home',
+    component: HomepageComponent,
+   canActivate: [AuthGuard]
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Task/src/app/app.module.ts
+++ Task/src/app/app.module.ts
@@ -1,18 +1,31 @@
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
